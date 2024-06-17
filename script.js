8f9a8a6afcfa9d5e9d62006cd4df6cdba825ab82
Muze <mark46716@gmail.com>
2024-06-17 00:40:36 -0300

    Modify display with any operators
    Previously, the display only actualized its value with the equal button.
    Now, when the user enter at least two numbers and then hits a operator
    button the display updates with the result and the selected operator.

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-const add = function(a, b) {
+const add = function(a, b = 0) {
 	return a + b;
 };
 
-const subtract = function(a, b) {
+const subtract = function(a, b = 0) {
 	return a - b;
 };
 
-const divide = function(a ,b) {
+const divide = function(a ,b = 1) {
 	return a / b;
 };
 
-const multiply = function(a, b) {
+const multiply = function(a, b = 1) {
   return a * b;
 };
 
 const operate = function (a, operator, b) {
+    a = parseInt(a);
+    b = parseInt(b);
     switch(operator) {
         case '+':
             return add(a, b);
@@ -24,41 +26,50 @@ const operate = function (a, operator, b) {
             return divide(a, b);
         case '*':
             return multiply(a, b);
-        default:
-            alert('Please enter a valid operator');
     }
 }
-/*
-let num1 = parseInt(prompt('Please enter a number: '));
-let num2 = parseInt(prompt('Please enter another number: '));
-let operator = prompt('Please enter the operator: ');
 
-console.log(operate(num1, num2, operator))
-*/
 const calculator = document.querySelector('.calculator');
 const display = document.querySelector('.display');
 let operation = '';
-console.log(calculator);
+let equalPressed = true;
+let result = 0;
 
 calculator.addEventListener('click', e => {
     if (e.target.tagName == 'BUTTON') { //Check if the elment clicked is a button
-        operation = display.textContent;
+
+        if (equalPressed) {  //If equal have been pressed deletes display value
+            equalPressed = false;
+            display.textContent = '';
+            
+        }
+
         switch(e.target.className) {
             case 'operator':
                 display.textContent += ` ${e.target.textContent} `;
+                operation = operation.split(' ');
+                if (operation[2]) { //Checks if the operations contains a second number
+                    result = operate(operation[0], operation[1], operation[2]);
+                    display.textContent = `${result} ${e.target.textContent} `; //Updates display with the result and the current operator selected
+                }
                 break;
+
             case 'equal': 
                 console.log(operation);
                 operation = operation.split(' ');
-                display.textContent = operate(operation[0], operation[1], operation[2])
+                result = operate(operation[0], operation[1], operation[2])
+                display.textContent = result;
+                equalPressed = true;
                 break;
+
             case 'clear':
                 display.textContent = '';
                 break;
+
             default:
                 display.textContent += `${e.target.textContent}`
                 break;  
             }
+            operation = display.textContent;
     }
-
 })
