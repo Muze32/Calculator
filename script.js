b4b9e2f9b787feb40f1b1c66bfe4b7195230da4a
Muze <mark46716@gmail.com>
2024-06-17 22:20:14 -0300

    Fix multiple floating points on a single number
    Change the behaviour of the floating button so that now it only allow
    a single floating point.
    To introduce a new floating point the user must first input an operator.

--- script.js
+++ script.js
@@ -50,12 +50,14 @@ const calculator = document.querySelector('.calculator');
 const display = document.querySelector('.display');
 let operation = '';
 let result = 0;
+let floatingEnabled = true;
 
 calculator.addEventListener('click', e => {
     if (e.target.tagName == 'BUTTON') { //Check if the elment clicked is a button
 
         switch(e.target.className) {
             case 'operator':
+                floatingEnabled = true; //Enables the floating button again
                 display.textContent += ` ${e.target.textContent} `;
                 operation = operation.split(' ');
                 if (operation[2]) { //Checks if the operations contains a second number
@@ -65,10 +67,9 @@ calculator.addEventListener('click', e => {
                 break;
 
             case 'equal': 
-                console.log(operation);
                 operation = operation.split(' ');
                 result = operate(operation[0], operation[1], operation[2]);
-                if (isNaN(result)) {
+                if (isNaN(result) && result != "lmao") {
                     break;
                 }
                 display.textContent = result;
@@ -76,6 +77,12 @@ calculator.addEventListener('click', e => {
 
             case 'clear':
                 display.textContent = '';
+                break;
+            case "floating":
+                if (floatingEnabled) { //Checks if the floating button has already been pressed
+                    display.textContent += `${e.target.textContent}`
+                    floatingEnabled = false;    
+                }
                 break;
 
             default:
