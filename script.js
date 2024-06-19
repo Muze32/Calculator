a9355563fd5af8097feff107c6437a7a500f7b7b
Muze <mark46716@gmail.com>
2024-06-18 22:50:48 -0300

    Add keyobard support and optimizes event listeners
    In the previous commit when the user clicked a button the main code
    executed; Now, it instead calls a function which works for both, button
    and keyboard inputs.

--- script.js
+++ script.js
@@ -48,52 +48,67 @@ const operate = function (a, operator, b) {
 
 const calculator = document.querySelector('.calculator');
 const display = document.querySelector('.display');
-let operation = '';
+const body = document.querySelector("body");
+let equation = '';
 let result = 0;
 let floatingEnabled = true;
 
+////////////Main code
 calculator.addEventListener('click', e => {
     if (e.target.tagName == 'BUTTON') { //Check if the elment clicked is a button
-
-        switch(e.target.className) {
-            case 'operator':
-                floatingEnabled = true; //Enables the floating button again
-                display.textContent += ` ${e.target.textContent} `;
-                operation = operation.split(' ');
-                if (operation[2]) { //Checks if the operations contains a second number
-                    result = operate(operation[0], operation[1], operation[2]);
-                    display.textContent = `${result} ${e.target.textContent} `; //Updates display with the result and the current operator selected
-                }
-                break;
-
-            case 'equal': 
-                operation = operation.split(' ');
-                result = operate(operation[0], operation[1], operation[2]);
-                if (isNaN(result) && result != "lmao") {
-                    break;
-                }
-                display.textContent = result;
-                break;
-
-            case 'clear':
-                display.textContent = '';
-                break;
-
-            case "floating":
-                if (floatingEnabled) { //Checks if the floating button has already been pressed
-                    display.textContent += `${e.target.textContent}`
-                    floatingEnabled = false;    
-                }
-                break;
-                
-            case 'backspace':
-                display.textContent = display.textContent.slice(0, -1);
-                break;
-
-            default:
-                display.textContent += `${e.target.textContent}`
-                break;  
-            }
-            operation = display.textContent;
+        updateDisplayContent(e.target.textContent);
     }
 })
+
+body.addEventListener('keydown', e => {
+    if (!(isNaN(e.key)) || ('/*+-=.').split('').includes(e.key) || ['Enter', 'Backspace'].includes(e.key)) {
+        updateDisplayContent(e.key);
+    }
+})
+
+
+function updateDisplayContent (event) {
+    switch(event) {
+        case '-':
+        case '+':
+        case '/':   
+        case '*':
+            floatingEnabled = true; //Enables the floating button again
+            display.textContent += ` ${event} `;
+            if (equation[2]) { //Checks if the equations contains a second number
+                result = operate(equation[0], equation[1], equation[2]);
+                display.textContent = `${result} ${event} `; //Updates display with the result and the current operator selected
+            }
+            break;
+
+        case '=': 
+        case 'Enter':
+            result = operate(equation[0], equation[1], equation[2]);
+            if (isNaN(result) && result != "lmao") {
+                break;
+            }
+            display.textContent = result;
+            break;
+
+        case 'AC':
+            display.textContent = '';
+            break;
+
+        case '.':
+            if (floatingEnabled) { //Checks if the floating button has already been pressed
+                display.textContent += `${event}`
+                floatingEnabled = false;    
+            }
+            break;
+            
+        case 'DEL':
+        case 'Backspace':
+            display.textContent = display.textContent.slice(0, -1);
+            break;
+
+        default:
+            display.textContent += `${event}`
+            break;  
+        }
+        equation = display.textContent.split(' ');
+}
